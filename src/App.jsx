6b62a1cb7ebd991575520cc379b7f66b8a3c4db1
doceStudio <cesar@docestudio.fr>
2024-01-12 15:07:01 +0100

    v1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Quiz from "./components/Quizz";
+function App() {
+  return (
+    <>
+      <Header />
+      <Quiz/>
+    </>
+  );
+}
+
+export default App;
